214fd6bf9235dd20d6a3d88438cd5907458467b8
Shannon-JXT <584901185@qq.com>
2019-09-16 16:34:49 +1000

    try to fix Azure static files issue

--- LKPLS/settings.py
+++ LKPLS/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    'https://linkinplus.azurewebsites.net/static',
+    os.path.join('https://linkinplus.azurewebsites.net/static', 'static'),
 ]
 
 STATICFILES_FINDERS = [
