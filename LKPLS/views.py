9616b66920cc345cf9eb8514f76dd1aa6d6f9f95
Shannon-JXT <584901185@qq.com>
2019-10-08 21:52:52 +1100

    remove print in views

--- LKPLS/views.py
+++ LKPLS/views.py
@@ -3,13 +3,10 @@ from django.contrib.auth import authenticate, login
 
 def userLogin(request):
     username = request.POST.get('username', '')
-    print("=============", username)
     password = request.POST.get('password', '')
-    print("----------------", password)
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        print("aaaaaaaa")
         return redirect('/')
     else:
         return render(request, 'error.html', {})
