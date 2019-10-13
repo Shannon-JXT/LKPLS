8c616f35c6ebc6f983c6bccb85013b1e11524143
Shannon-JXT <584901185@qq.com>
2019-10-13 20:20:30 +1100

    update home page title

--- Immigrant/views.py
+++ Immigrant/views.py
@@ -5,28 +5,8 @@ import json
 
 # Create your views here.
 def au_display(request):
-    data = Region.objects.all()
-    year1996 = {'time': 1996}
-    year2001 = {'time': 2001}
-    year2006 = {'time': 2006}
-    year2011 = {'time': 2011}
-    year2016 = {'time': 2016}
-    for item in data:
-        name = str(item.country_name)
-        if name == 'Australia':
-            if int(item.year) == 1996:
-                year1996[name] = item.migrant_num
-            if int(item.year) == 2001:
-                year2001[name] = item.migrant_num
-            if int(item.year) == 2006:
-                year2006[name] = item.migrant_num
-            if int(item.year) == 2011:
-                year2011[name] = item.migrant_num
-            if int(item.year) == 2016:
-                year2016[name] = item.migrant_num
-
-    dic = {1996: year1996, 2001: year2001, 2006: year2006, 2011: year2011, 2016: year2016}
-    return render(request, "au_info.html", {'au_trends': json.dumps(dic)})
+    context = {}
+    return render(request, "au_info.html", context)
 
 def cn_display(request):
     data = Region.objects.all()
